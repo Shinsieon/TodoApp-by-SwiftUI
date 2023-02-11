e0de5f337b5192217b83a1db027164976de9d869
Shin Sie On <coolguysiun@gmail.com>
2023-02-11 17:43:40 +0900

    Initial Commit

---
+++ myTodoApp/myTodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myTodoAppApp.swift
+//  myTodoApp
+//
+//  Created by 신시언 on 2023/02/11.
+//
+
+import SwiftUI
+
+@main
+struct myTodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
